1df5feb2f3b8e5e900155244932cd73cbba91c12
gatewe <macko.martin77@gmail.com>
2023-04-25 21:53:51 +0200

    changes in author layout

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -9,8 +9,8 @@ export default function AuthorLayout({ children, frontMatter }) {
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y">
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8">
+        <div className="flex-start space-y-2 xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-y-0 pt-8 pb-8">
+          <div className="flex flex-col col-span-2 mr-auto items-center">
             <Image
               src={avatar}
               alt="avatar"
@@ -28,7 +28,31 @@ export default function AuthorLayout({ children, frontMatter }) {
               <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
-          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          {/* <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div> */}
+          <div className="xl:translate-y-1/4 xl:-translate-x-1/3 col-span-2">
+            <blockquote className="relative">
+              <svg
+                class="absolute top-0 left-0 h-16 w-16 -translate-x-6 -translate-y-8 transform text-gray-100 dark:text-gray-700"
+                width="24"
+                height="27"
+                viewBox="0 0 24 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                  fill="currentColor"
+                />
+              </svg>
+
+              <div className="relative z-10">
+                <span className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                  I do research so you don't have to.
+                </span>
+              </div>
+            </blockquote>
+          </div>
         </div>
       </div>
     </>
--- pages/authors/martinmacko.js
+++ pages/authors/martinmacko.js
@@ -23,56 +23,63 @@ export default function MartinMacko({ authorDetails, posts }) {
   return (
     <>
       {/* <AuthorsNavLayout> */}
+      <div>
         <div>
-          <div>
-            <MDXLayoutRenderer
-              layout={frontMatter.layout || DEFAULT_LAYOUT}
-              mdxSource={mdxSource}
-              frontMatter={frontMatter}
-            />
-            <div className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 pt-12 pb-4">Posts by Martin</div>
-            <ul>
-              {!posts.length && 'No posts found.'}
-              {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-                const { category, slug, date, title, summary, tags } = frontMatter
-                if (category == 'Azure security') {
-                  return (
-                    <li key={slug} className="py-4">
-                      <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                        <dl>
-                          <dt className="sr-only">Published on</dt>
-                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                            <time dateTime={date}>{formatDate(date)}</time>
-                          </dd>
-                        </dl>
-                        <div className="space-y-3 xl:col-span-3">
-                          <div>
-                            <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                              <Link
-                                href={`/blog/${slug}`}
-                                className="text-gray-900 dark:text-gray-100"
-                              >
-                                {title}
-                              </Link>
-                            </h3>
-                            <div className="flex flex-wrap">
-                              {tags.map((tag) => (
-                                <Tag key={tag} text={tag} />
-                              ))}
-                            </div>
-                          </div>
-                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                            {summary}
+          <MDXLayoutRenderer
+            layout={frontMatter.layout || DEFAULT_LAYOUT}
+            mdxSource={mdxSource}
+            frontMatter={frontMatter}
+          />
+          {/* <div className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400 pt-8 pb-4">Posts by Martin</div> */}
+          <div className="pt-4 pb-4">
+            <fieldset class="border-t-[0.5px] border-gray-700">
+              <legend class="ml-auto pl-4 text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                Posts by Martin
+              </legend>
+            </fieldset>
+          </div>
+          <ul>
+            {!posts.length && 'No posts found.'}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { category, slug, date, title, summary, tags } = frontMatter
+              if (category == 'Azure security') {
+                return (
+                  <li key={slug} className="py-4">
+                    <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-3 xl:col-span-3">
+                        <div>
+                          <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h3>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
                           </div>
                         </div>
-                      </article>
-                    </li>
-                  )
-                }
-              })}
-            </ul>
-          </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                    </article>
+                  </li>
+                )
+              }
+            })}
+          </ul>
         </div>
+      </div>
       {/* </AuthorsNavLayout> */}
     </>
   )
