cd6a051ea558fd695e27b1123c8cd9b165da3174
gatewe <macko.martin77@gmail.com>
2023-05-02 23:10:28 +0200

    hero header color adjustments

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="max-w h-[500px] overflow-hidden rounded-lg border border-gray-200 bg-opacity-0 bg-[url('/static/hackers-resized-min-light3.svg')] bg-bottom shadow-[0_0_12px_rgba(0,0,0,0.6)] shadow-fuchsia-400/20 dark:border-gray-700 dark:bg-[url('/static/hackers-resized-min-dark.svg')]">
+    <div className="max-w h-[500px] overflow-hidden rounded-lg border border-gray-200 bg-opacity-0 bg-[url('/static/hackers-resized-min-light2.svg')] bg-bottom shadow-[0_0_12px_rgba(0,0,0,0.6)] shadow-fuchsia-400/20 dark:border-gray-700 dark:bg-[url('/static/hackers-resized-min-dark.svg')]">
       <div className="dark:bg-gray-700 bg-gray-600 pt-2 pb-2">
         <div className="position-absolute ml-4">
           <div className="mr-2 inline-block h-3 w-3 rounded-full bg-red-500"></div>
@@ -11,7 +11,7 @@ export default function Hero() {
       <div className="min-w-screen m-4 grid basis-full items-center justify-center">
         <div className="flex flex-col items-start justify-between xl:flex-row">
           <div className="flex flex-col text-4xl sm:text-5xl md:text-6xl">
-            <span className="bg-gradient-to-r dark:from-purple-400 dark:to-pink-600  from-purple-300 to-pink-500 bg-clip-text font-semibold uppercase text-transparent">
+            <span className="bg-gradient-to-r dark:from-purple-400 dark:to-pink-600  from-purple-400 to-pink-600 bg-clip-text font-semibold uppercase text-transparent">
               Welcome to Compaas!
             </span>
             <p className="text-white type-writer font-semibold uppercase">
