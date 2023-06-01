1e7e8206f510be76187f0e1f6ac9aa7c92c0348b
gatewe <macko.martin77@gmail.com>
2023-06-01 22:56:57 +0200

    update to header.js

--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,13 @@ export default function Header() {
               <div className="mr-3">
                 <Logo className="fill-current" />
               </div>
-              {typeof siteMetadata.headerTitle === 'string' ? (
+              {/* {typeof siteMetadata.headerTitle === 'string' ? (
                 <div className="hidden h-6 text-2xl font-semibold sm:block">
                   {siteMetadata.headerTitle}
                 </div>
               ) : (
                 siteMetadata.headerTitle
-              )}
+              )} */}
             </div>
           </Link>
         </div>
